214f1703b796567277623d1cc8af268b0151ceaa
alisieck <alisieck@GDNM0162>
2016-10-05 21:00:53 +0200

    Add project files.

---
+++ MP3Player/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MP3Player
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.playButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.autostartCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(177, 172);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Otwórz";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(312, 172);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Graj";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(239, 240);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(63, 89);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(407, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(111, 287);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(309, 45);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // autostartCheckBox
+            // 
+            this.autostartCheckBox.AutoSize = true;
+            this.autostartCheckBox.Location = new System.Drawing.Point(177, 202);
+            this.autostartCheckBox.Name = "autostartCheckBox";
+            this.autostartCheckBox.Size = new System.Drawing.Size(67, 17);
+            this.autostartCheckBox.TabIndex = 5;
+            this.autostartCheckBox.Text = "autostart";
+            this.autostartCheckBox.UseVisualStyleBackColor = true;
+            this.autostartCheckBox.CheckedChanged += new System.EventHandler(this.autostartCheckBox_CheckedChanged);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 373);
+            this.Controls.Add(this.autostartCheckBox);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox autostartCheckBox;
+    }
+}
+
---
+++ MP3Player/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP3Player
+{
+    public partial class MainWindow : Form
+    {
+        private WMPLib.WindowsMediaPlayer wplayer; // przenioslem inicjacje do konstruktora
+
+        public MainWindow() // to jest konstruktor
+        {
+            InitializeComponent();
+            this.wplayer = new WMPLib.WindowsMediaPlayer(); // tworze nowy obiekt playera
+            wplayer.settings.autoStart = false;
+            autostartCheckBox.Checked = wplayer.settings.autoStart; // przypisalem zaznaczenie checkboxa do wartosci autostartu - jak zmienisz autostart to zobaczymy to w checkboxie
+            // to jest o tyle fajne, że pole "Checked" dostaje referencję do tego samego obiektu co "autoStart" więc zmiany na obu polach to zmiany w tym samym obiekcie
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            String filePath = "";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                Console.WriteLine(filePath);
+
+                wplayer.URL = filePath;
+                
+           
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.play();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.pause();
+        }
+
+        private void autostartCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            wplayer.settings.autoStart = !wplayer.settings.autoStart; // zamieniam na autostart na wartosc przeciwna
+        }
+    }
+}
